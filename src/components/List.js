9ac3f9a1df9900bd71cd2b5f4e4b2d0d77b92a30
Freja <frej400d@stud.kea.dk>
2022-04-19 19:10:03 +0200

    components

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+import Form from "./Form";
+
+export default function List(props) {
+  return (
+    <section>
+      <h2>{props.header}</h2>
+      <ul>
+        <Card />
+        <Card />
+      </ul>
+      <Form />
+    </section>
+  );
+}
