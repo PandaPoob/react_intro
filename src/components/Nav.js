4c6974f02c86daada9853ae57f8a3e22b63bc654
Freja <frej400d@stud.kea.dk>
2022-04-20 22:31:44 +0200

    keys

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Nav() {
+  //const navigation = [<a href="#">Home</a>, <a href="#">Contact us</a>];
+  //const buttons = [<Button></Button>, <Button></Button>];
   return <nav>Nav</nav>;
 }
